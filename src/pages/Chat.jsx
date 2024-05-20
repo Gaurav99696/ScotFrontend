d3d858af3b7e58e3a220d55146b5c0b368833b2e
gauravMishra <gauravmishra99696@gmail.com>
2024-05-20 21:32:44 +0530

    error fix

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -18,7 +18,7 @@ const Chat = () => {
   const userName = cookie.get("Scot_Auth-User_Data");
   const userData = cookie.get("Scot_Auth-User_Data");
 
-  const socket = io("http://localhost:6000");
+  const socket = io("https://scotbackend.onrender.com");
 
   useEffect(() => {
     setGettingCookie(cookie.get("Scot_Auth-Token"));
