1ba8ccf91846585ab58435553c6816c678e1ade0
gauravMishra <gauravmishra99696@gmail.com>
2024-05-27 22:12:54 +0530

    Delete Acc funcsenlity

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
+import React from "react";
 import Cookies from "universal-cookie";
 import { useNavigate } from "react-router-dom";
 
+const Profile = ({ userName, email }) => {
+  const cookie = new Cookies(null, "/");
+  const navigate = useNavigate();
 
-const Profile = ({userName, email}) => {
+  const signOut = () => {
+    cookie.remove("Scot_Auth-User_Data");
+    cookie.remove("Scot_Auth-Token");
+    navigate("/");
+  };
 
-    const cookie = new Cookies(null, "/");
-    const navigate = useNavigate();
+  const deleteAcc = async () => {
+    const userName = cookie.get("Scot_Auth-User_Data").userName
+    const deleteAcc = await fetch(
+      `https://scotbackend.onrender.com/api/users/deleteUser/${userName}`
+    );
+    // cookie.remove("Scot_Auth-User_Data");
+    // cookie.remove("Scot_Auth-Token");
+    // navigate("/");
+  };
 
+  return (
+    <div className="profile">
+      <img
+        src="https://t4.ftcdn.net/jpg/04/98/72/43/360_F_498724323_FonAy8LYYfD1BUC0bcK56aoYwuLHJ2Ge.jpg"
+        alt="User"
+      />
+      <h4>{userName}</h4>
+      <h5>{email}</h5>
+      <button className="oprationButtons" onClick={(e) => signOut()}>
+        Sign Out
+      </button>
+      <button
+        className="oprationButtons"
+        onClick={(e) => deleteAcc()}
+        id="deleteAcc"
+      >
+        Delete Account
+      </button>
+    </div>
+  );
+};
 
-    const signOut = () => {
-        cookie.remove("Scot_Auth-User_Data")
-        cookie.remove("Scot_Auth-Token")
-        navigate("/");
-    }
-
-    return (
-        <div className='profile'>
-            <img src="https://t4.ftcdn.net/jpg/04/98/72/43/360_F_498724323_FonAy8LYYfD1BUC0bcK56aoYwuLHJ2Ge.jpg" alt="User"/>
-            <h4>{userName}</h4>
-            <h5>{email}</h5>
-            <button className='oprationButtons' onClick={e => signOut()}>Sign Out</button>
-            {/* <button className='oprationButtons' id='deleteAcc'>Delete Account</button> */}
-        </div>
-    )
-}
-
-export default Profile
+export default Profile;
